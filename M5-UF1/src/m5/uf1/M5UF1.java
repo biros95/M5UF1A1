5ff1c564bf358a066d28995fc756918315979a73
biros95 <biros1995@gmail.com>
2016-10-07 20:03:08 +0200

    Add files via upload

---
+++ M5-UF1/src/m5/uf1/M5UF1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m5.uf1;
+
+/**
+ *
+ * @author MarcosPortatil
+ */
+public class M5UF1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
